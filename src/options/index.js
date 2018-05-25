fae0c1d5f8746f7c1469d69ebdb3b81f3156697e
Saiya <evecalm@live.com>
2018-05-25 18:58:56 +0800

    require element components on demand

--- src/options/index.js
+++ src/options/index.js
@@ -1,29 +1,66 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import ElementUI from 'element-ui'
+import {
+  Select,
+  Option,
+  Checkbox,
+  Dialog,
+  Button,
+  RadioGroup,
+  Radio,
+  Tabs,
+  Col,
+  Row,
+  Form,
+  FormItem,
+  Input,
+  TabPane,
+  Popover,
+  Table,
+  TableColumn,
+  Switch
+} from 'element-ui'
 import enLocale from 'element-ui/lib/locale/lang/en'
-import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+import elLocale from 'element-ui/lib/locale'
 import i18n from '@/common/i18n'
 import locales from '@/common/locales'
-import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
 import App from './main'
 import router from './router'
 import titlebar from './components/titlebar'
 
+elLocale.use(enLocale)
 Vue.use(VueI18n)
-Vue.use(ElementUI)
 
 Vue.config.lang = i18n.lang
-// Vue.config.lang = 'zen'
+// Vue.config.lang = 'en'
 
 // translate text out of vue app
 document.documentElement.innerHTML = i18n.internationalize(
   document.documentElement.innerHTML
 )
 
-Vue.locale('zh-cn', zhLocale)
-Vue.locale('en', enLocale)
+const components = [
+  Select,
+  Option,
+  Checkbox,
+  Dialog,
+  Button,
+  RadioGroup,
+  Radio,
+  Tabs,
+  Col,
+  Row,
+  Form,
+  FormItem,
+  Input,
+  TabPane,
+  Popover,
+  Table,
+  TableColumn,
+  Switch
+]
+components.forEach(c => Vue.use(c))
 
 // register custom locale
 for (var key in locales) {
